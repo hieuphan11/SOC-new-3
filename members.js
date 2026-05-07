656e2d77ca14440ea22723eeafb6f78cbaefb1ab
hieuphan11 <hieu.phan1132@gmail.com>
2026-05-07 22:06:34 +0700

    Add files via upload

---
+++ members.js
@@ -0,0 +1,100 @@
+// ============================================================
+//  FILE DỮ LIỆU HỘI VIÊN CLB
+//  Thêm hội viên mới: copy 1 block {}, sửa thông tin,
+//  đặt id dạng: ten-khong-dau-so-thu-tu  (vd: le-van-c-8)
+// ============================================================
+
+var members = {
+
+  "vu-ngoc-cuong-1": {
+    id:         "vu-ngoc-cuong-1",
+    stt:        1,
+    ten:        "Vũ Ngọc Cường",
+    sdt:        "0938688679",
+    ngaysinh:   "27/06/1985",
+    cccd:       "15085011371",
+    diachi:     "Số 10 lô O, đường 14 - Hoàng Quốc Việt, KDC Phú Mỹ, P. Tân Mỹ, TP.HCM",
+    nghenghiep: "GĐ - Cty CP Sứ Kỹ Thuật Hoàng Liên Sơn Sài Gòn",
+    biensoxe:   "51K-808.79",
+    avatar:     ""
+  },
+
+  "nguyen-luong-binh-2": {
+    id:         "nguyen-luong-binh-2",
+    stt:        2,
+    ten:        "Nguyễn Lương Bình",
+    sdt:        "0903814516",
+    ngaysinh:   "12/04/1967",
+    cccd:       "15067500592",
+    diachi:     "09 Hưng Thái 1, Phú Mỹ Hưng, P. Tân Hưng, TP.HCM",
+    nghenghiep: "Cty CP Tiến Việt",
+    biensoxe:   "51D-359.63",
+    avatar:     ""
+  },
+
+  "phan-xuan-hai-3": {
+    id:         "phan-xuan-hai-3",
+    stt:        3,
+    ten:        "Phan Xuân Hải",
+    sdt:        "0908295959",
+    ngaysinh:   "16/09/1965",
+    cccd:       "22065008923",
+    diachi:     "Số 23 đường 28, KP2, P. Bình Trưng, TP.HCM",
+    nghenghiep: "Cán bộ hưu trí",
+    biensoxe:   "(Không có)",
+    avatar:     ""
+  },
+
+  "ho-thanh-tuan-4": {
+    id:         "ho-thanh-tuan-4",
+    stt:        4,
+    ten:        "Hồ Thanh Tuấn",
+    sdt:        "0933888818",
+    ngaysinh:   "15/12/1976",
+    cccd:       "94076000001",
+    diachi:     "54 Nguyễn Thị Thập, P. Tân Hưng, TP.HCM",
+    nghenghiep: "Cty CP Ngọc Trai Hoàng Gia",
+    biensoxe:   "51B-516.91",
+    avatar:     ""
+  },
+
+  "pham-sy-trung-5": {
+    id:         "pham-sy-trung-5",
+    stt:        5,
+    ten:        "Phạm Sỹ Trung",
+    sdt:        "0903824985",
+    ngaysinh:   "20/05/1967",
+    cccd:       "15067004553",
+    diachi:     "16F Phùng Khắc Khoan, P. Đa Kao, TP.HCM",
+    nghenghiep: "Kiến trúc sư",
+    biensoxe:   "51G-758.20",
+    avatar:     ""
+  },
+
+  "nguyen-quoc-bao-6": {
+    id:         "nguyen-quoc-bao-6",
+    stt:        6,
+    ten:        "Nguyễn Quốc Bảo",
+    sdt:        "0973734242",
+    ngaysinh:   "19/05/1968",
+    cccd:       "45068009268",
+    diachi:     "16F 3/2 Hoàng Diệu, P. Phú Nhuận, TP.HCM",
+    nghenghiep: "Kiến trúc sư",
+    biensoxe:   "51K-098.76",
+    avatar:     ""
+  },
+
+  "tran-cong-toan-7": {
+    id:         "tran-cong-toan-7",
+    stt:        7,
+    ten:        "Trần Công Toàn",
+    sdt:        "0945626369",
+    ngaysinh:   "15/03/1990",
+    cccd:       "89090024416",
+    diachi:     "Mỹ Phú, Châu Phú, tỉnh An Giang",
+    nghenghiep: "Kỹ sư điện tử",
+    biensoxe:   "51D-790.62",
+    avatar:     ""
+  }
+
+};
